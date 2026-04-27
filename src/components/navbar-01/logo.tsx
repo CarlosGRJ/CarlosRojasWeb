eebab5dab723bdc01e4a1d9913a625a30787dd05
CarlosGRJ <carlosgrj@outlook.com>
2026-04-27 13:50:39 -0600

    refactor(logo): improve component structure and readability

--- src/components/navbar-01/logo.tsx
+++ src/components/navbar-01/logo.tsx
@@ -1,28 +1,26 @@
 'use client';
 
-import { ThemeEnum } from '@/types/theme';
+import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
+import { ThemeEnum } from '@/types/theme';
 import LogoBlack from '../svgs/LogoBlack';
 import LogoWhite from '../svgs/LogoWhite';
-import { useEffect, useState } from 'react';
 
 export type LogoProps = {
-  className?: string;
+  readonly className?: string;
 };
 
 export function Logo({ className }: LogoProps) {
-  const { theme } = useTheme();
-  const [hasMounted, setHasMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
+  useEffect(() => setMounted(true), []);
 
-  return !hasMounted ? (
-    <LogoBlack className={className} />
-  ) : theme === ThemeEnum.LIGHT ? (
-    <LogoBlack className={className} />
-  ) : (
+  if (!mounted) return <LogoBlack className={className} />;
+
+  return resolvedTheme === ThemeEnum.DARK ? (
     <LogoWhite className={className} />
+  ) : (
+    <LogoBlack className={className} />
   );
 }
